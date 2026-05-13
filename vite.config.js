75f3452de4b83246c8c6129feae20934afa4ab30
zzckyy <zackymm2080@gmail.com>
2026-05-13 20:23:10 +0700

    feat: tauri integrations

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,45 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+
+  server: {
+    // Tauri expects a fixed port, fail if that port is not available
+    port: 5173,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 5174,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+
+  // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code
+  envPrefix: ["VITE_", "TAURI_ENV_*"],
+
   build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM === "windows"
+        ? "chrome105"
+        : "safari13",
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
